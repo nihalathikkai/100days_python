0f587333e52d4e8b1219eac432f2c4695bc26960
nihalathikkai <nihalathikkai1999@gmail.com>
2023-02-10 18:59:46 +0530

    day30

--- day29_intermediate_password_manager/main.py
+++ day29_intermediate_password_manager/main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import messagebox
 from random import randint, choice, shuffle
+import json
 
 LOGO_IMAGE = "D:/python/100days/day29_intermediate_password_manager/logo.png"
-SAVE_FILE = "D:/python/100days/day29_intermediate_password_manager/data.txt"
+SAVE_FILE = "D:/python/100days/day29_intermediate_password_manager/data.json"
 
 # ASCII values:
 # 0-9: 48-57
@@ -35,6 +36,24 @@ def generate_password():
     root.clipboard_clear()
     root.clipboard_append(new_password)
     
+# ---------------------------- FIND PASSWORD  ------------------------------- #
+def search_password():
+    website = website_input.get().strip()
+    if not website: return
+    try: 
+        with open(SAVE_FILE, mode='r') as data_file:
+            data = json.load(data_file)            
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        messagebox.showinfo(title="Error", message=f"Data file not found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            if messagebox.askokcancel(title=website, message=f"Email:\t\t{email}\nPassword:\t{password}"):
+                root.clipboard_clear()
+                root.clipboard_append(password)
+        else:
+            messagebox.showinfo(title="Password not Found", message=f"Details for {website} not found")
     
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
@@ -53,10 +72,28 @@ def save_password():
         return
         
     is_ok = messagebox.askokcancel(title=f"Add password for {website}", message=f"Email:\t\t{email}\nPassword:\t{password}\n\nClick ok to save")
-    if is_ok:
-        output = f"{website}|{email}|{password}\n"
-        with open(SAVE_FILE, mode='a') as data:
-            data.write(output)
+    # is_ok = True
+    if is_ok:        
+        new_data = {
+            website: {
+                "email": email, 
+                "password": password
+                }
+            }
+        
+        try: 
+            with open(SAVE_FILE, mode='r') as data_file:
+                data = json.load(data_file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            data = new_data
+        # except Exception as e: print(e, type(e))
+        else:
+            data.update(new_data)
+            # data[website] = new_data[website]
+        finally:
+            with open(SAVE_FILE, mode='w') as data_file:
+                json.dump(data, data_file, indent=4)
+        
         website_input.delete(0, tk.END)
         password_input.delete(0, tk.END)
     
@@ -84,24 +121,26 @@ password_label.grid(row=3, column=0)
 
 
 # Entries
-website_input = tk.Entry(root, width=40)
+website_input = tk.Entry(root, width=30)
 website_input.focus()
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1)
 
-email_input = tk.Entry(root, width=40)
+email_input = tk.Entry(root, width=49)
 email_input.insert(0, "exapmledummy@gmail.com")
 email_input.grid(row=2, column=1, columnspan=2)
 
-password_input = tk.Entry(root, width=21)
+password_input = tk.Entry(root, width=30)
 password_input.grid(row=3, column=1)
 
 
 #Buttons
-generate_button = tk.Button(root, text="Generate Password", command=generate_password)#, width=15)
+search_button = tk.Button(root, text="Search", command=search_password, width=15)
+search_button.grid(row=1, column=2)
+
+generate_button = tk.Button(root, text="Generate", command=generate_password, width=15)
 generate_button.grid(row=3, column=2)
 
-add_button = tk.Button(root, text="Add", width=34, command=save_password)
+add_button = tk.Button(root, text="Add", width=42, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
-
 
 root.mainloop()
