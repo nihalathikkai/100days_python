b952476ef4755f8baea06c176e54f84d2672bfd1
nihalathikkai <nihalathikkai1999@gmail.com>
2023-03-31 19:21:18 +0530

    day38

--- day38_intermediate+_GoogleSheets/env.py
+++ day38_intermediate+_GoogleSheets/env.py
@@ -2,3 +2,10 @@
 APP_ID = "79e06a11"
 API_KEY = "e2ad3927da0b4a732354b1f155a6e889"
 BEARER = "BTUzC1Lsb6T5NBYQlGlP48HMsM5JXE6BiHVFvqouBicV4nl9S4I2yI1NsGzoHE24"
+
+
+
+# https://developer.nutritionix.com/admin/access_details
+# https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit#
+# https://dashboard.sheety.co/projects/6425dd340098f2649bd9f82b/auth
+# https://docs.google.com/spreadsheets/d/1wBev1SLAlABzVmsF0HOUNCV8xEkfKpLs_GWkdwpjU_4/edit#gid=0
